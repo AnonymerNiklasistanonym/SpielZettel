e4c751854d055999e8af3ff2fc9df11077164bd7
Niklas <niklas.mikeler@gmail.com>
2025-01-01 23:13:33 +0100

    Add initial project using npx create-next-app@latest spiel-zettel
    ✔ Would you like to use TypeScript? … Yes
    ✔ Would you like to use ESLint? … Yes
    ✔ Would you like to use Tailwind CSS? … No
    ✔ Would you like your code inside a `src/` directory? … Yes
    ✔ Would you like to use App Router? (recommended) … Yes
    ✔ Would you like to use Turbopack for `next dev`? … Yes
    ✔ Would you like to customize the import alias (`@/*` by default)? … No
    
    Then update the next.config.ts:
    
    {output: 'export'}

---
+++ spiel-zettel/README.md
@@ -0,0 +1,17 @@
+# SpielZettel
+
+## Getting Started
+
+Run the development server and visit [http://localhost:3000](http://localhost:3000):
+
+```bash
+npm run dev
+```
+
+## Export
+
+Export a static generated website in the `build` directory:
+
+```bash
+npm run build
+```
---
+++ spiel-zettel/next.config.ts
@@ -0,0 +1,13 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  // Configuration for a static export (Static Site Generation SSG)
+  output: 'export',
+  distDir: 'build',
+  images: {
+    unoptimized: true, // Disable Image Optimization for compatibility
+  },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/my-static-site/' : undefined, // Custom URL prefix
+};
+
+export default nextConfig;
