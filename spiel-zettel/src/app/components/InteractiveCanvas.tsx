2fb4ab9c2064a448a6279fa6db1f275a05a504c6
Niklas <niklas.mikeler@gmail.com>
2025-01-04 21:25:45 +0100

    WIP Implement Canvas 4
    Fix favicon to use the computed one.
    Refresh canvas automatically.
    Only fetch the image every file load instead of every render.

--- spiel-zettel/src/app/components/InteractiveCanvas.tsx
+++ spiel-zettel/src/app/components/InteractiveCanvas.tsx
@@ -1,19 +1,27 @@
-import {useState, useRef, useEffect, useCallback} from "react";
-import {readSpielZettelFile} from "../helper/readSpielZettelFile";
-import type {ChangeEvent, MouseEvent as ReactMouseEvent} from "react";
-import type {SpielZettelFileData} from "../helper/readSpielZettelFile";
-import {render, type SpielZettelElementInfoState} from "../helper/renderSpielZettel";
+import { useState, useRef, useEffect, useCallback, useMemo } from "react";
+import { readSpielZettelFile } from "../helper/readSpielZettelFile";
+import type { ChangeEvent, MouseEvent as ReactMouseEvent } from "react";
+import type { SpielZettelFileData } from "../helper/readSpielZettelFile";
+import { render, type SpielZettelElementInfoState } from "../helper/renderSpielZettel";
+import { handleInputs } from "../helper/handleSpielZettelInputs";
 
 function isFileHandle(handle: FileSystemHandle): handle is FileSystemFileHandle {
-    return handle.kind === "file";
+  return handle.kind === "file";
 }
 
 function InteractiveCanvas() {
   const [file, setFile] = useState<File | null>(null);
+  const [image, setImage] = useState<HTMLImageElement | null>(null);
   const [elementInfoState, setElementInfoState] = useState<SpielZettelElementInfoState[] | null>(null);
   const [spielZettelData, setSpielZettelData] = useState<SpielZettelFileData | null>(null);
   const [debug, setDebug] = useState(false);
+  const [refresh, setRefresh] = useState(true);
+  const [overlayVisible, setOverlayVisible] = useState(false);
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+  const toggleOverlay = useCallback(() => {
+    setOverlayVisible(!overlayVisible);
+  }, [overlayVisible]);
 
   useEffect(() => {
     if ('serviceWorker' in navigator) {
@@ -50,8 +58,9 @@ function InteractiveCanvas() {
     if (!event.target.files) return;
     const uploadedFile = event.target.files[0];
     if (uploadedFile) {
-        console.log("File received (upload):", uploadedFile);
+      console.log("File received (upload):", uploadedFile);
       setFile(uploadedFile);
+      setOverlayVisible(false);
     }
   };
 
@@ -78,11 +87,16 @@ function InteractiveCanvas() {
 
   useEffect(() => {
     if (file !== null) {
-        readSpielZettelFile(file).then(data => {
-            console.log(data);
-            setSpielZettelData(data);
-            setElementInfoState(data.dataJSON.elements ?? null);
-        })
+      readSpielZettelFile(file).then(data => {
+        console.log(data);
+        setSpielZettelData(data);
+        setElementInfoState(data.dataJSON.elements ?? null);
+        const img = new Image();
+        img.src = data.imageBase64;
+        img.onload = () => {
+          setImage(img); // Store the image in state
+        };
+      })
     }
   }, [file])
 
@@ -100,8 +114,8 @@ function InteractiveCanvas() {
 
   const drawCanvas = useCallback(() => {
     const canvas = canvasRef.current;
-    console.log("drawCanvas", canvas, canvas?.width, canvas?.height)
-    if (canvas === null || spielZettelData === null || elementInfoState === null) return;
+    console.log("drawCanvas", canvas, canvas?.width, canvas?.height, window.innerWidth, window.innerHeight)
+    if (canvas === null || spielZettelData === null || elementInfoState === null || image === null) return;
     const ctx = canvas.getContext("2d");
     if (ctx === null) return;
 
@@ -109,88 +123,27 @@ function InteractiveCanvas() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     // Draw the image centered
-    const image = new Image();
-    image.src = spielZettelData.imageBase64;
-    image.onload = () => {
-      render(canvas, ctx, image, elementInfoState, debug);
-      /*
-      const canvasWidth = canvas.width;
-      const canvasHeight = canvas.height;
-      const imgWidth = image.width;
-      const imgHeight = image.height;
-      console.log("drawImage1", image, image.width, image.height);
+    const newElements = render(canvas, ctx, image, elementInfoState, debug);
+    setElementInfoState(newElements);
+  }, [debug, elementInfoState, image, spielZettelData]);
 
-      const scale = Math.min(
-        canvasWidth / imgWidth,
-        canvasHeight / imgHeight
-      );
-      const imgX = (canvasWidth - imgWidth * scale) / 2;
-      const imgY = (canvasHeight - imgHeight * scale) / 2;
-
-      // Draw the image centered
-      ctx.drawImage(image, imgX, imgY, imgWidth * scale, imgHeight * scale);
-      console.log("drawImage2", image, imgX, imgY, imgWidth * scale, imgHeight * scale);
-      */
-
-      /*
-      // Draw checkmark
-      if (showCheckmark) {
-        ctx.font = "48px Arial";
-        ctx.fillStyle = "green";
-        ctx.textAlign = "center";
-        ctx.textBaseline = "middle";
-        ctx.fillText("X", canvas.width / 2, canvas.height / 2);
-      }
-
-      // Draw number
-      if (demoNumber !== null) {
-        ctx.font = "36px Arial";
-        ctx.fillStyle = "blue";
-        ctx.textAlign = "center";
-        ctx.textBaseline = "top";
-        ctx.fillText(`${demoNumber}`, canvas.width / 2, canvas.height / 2 + 50);
-      }
-      */
-    }
-  }, [debug, elementInfoState, spielZettelData]);
-
-  const handleCanvasClick = (event: ReactMouseEvent<HTMLCanvasElement, MouseEvent>) => {
+  const handleCanvasClick = useCallback((event: ReactMouseEvent<HTMLCanvasElement, MouseEvent>) => {
+    console.log("handleCanvasClick");
     const canvas = canvasRef.current;
-    if (canvas === null) return;
-    /*
-    const rect = canvas.getBoundingClientRect();
-    const x = event.clientX - rect.left;
-    const y = event.clientY - rect.top;
+    if (canvas === null || spielZettelData === null || elementInfoState === null || image === null) return;
 
-    const canvasWidth = canvas.width;
-    const canvasHeight = canvas.height;
-
-    // Check if the click is in the center for the checkmark
-    const centerRadius = 50; // Clickable radius around the center
-    const distToCenter = Math.sqrt(
-      Math.pow(x - canvasWidth / 2, 2) + Math.pow(y - canvasHeight / 2, 2)
-    );
-    if (distToCenter <= centerRadius) {
-      setShowCheckmark((prev) => !prev);
-    }
-
-    // Check if the click is in the bottom area for number input
-    if (y > canvasHeight / 2 + 100 && y < canvasHeight / 2 + 150) {
-      const inputNumber = prompt("Enter a number:");
-      if (inputNumber !== null) {
-        setNumber(Number(inputNumber));
-      }
-    }
-    */
-  };
+    const newElements = handleInputs(canvas, image, event, elementInfoState, debug, setRefresh);
+    setElementInfoState(newElements);
+  }, [debug, elementInfoState, spielZettelData, image]);
 
   useEffect(() => {
     if (canvasRef.current) {
       canvasRef.current.width = window.innerWidth;
       canvasRef.current.height = window.innerHeight;
     }
+    setRefresh(false);
     drawCanvas();
-  }, [spielZettelData, elementInfoState, debug, drawCanvas]);
+  }, [spielZettelData, elementInfoState, debug, refresh, drawCanvas]);
 
   useEffect(() => {
     const resizeCanvas = () => {
@@ -214,24 +167,110 @@ function InteractiveCanvas() {
     };
   }, [spielZettelData, elementInfoState, drawCanvas]);
 
+  const name = useMemo<string | null>(() => {
+    if (spielZettelData === null) return null;
+    const info = spielZettelData.dataJSON;
+    return `${info.name} (v${info.version.major}.${info.version.minor}.${info.version.patch})`;
+  }, [spielZettelData]);
+
+  const debugString = useMemo<string | null>(() => {
+    const canvas = canvasRef.current;
+    if (canvas === null) return null;
+    return `canvasSize=${canvas?.width}x${canvas?.height}, windowSize=${window.innerWidth}x${window.innerHeight}, screenSize=${window.screen.width}x${window.screen.height}`;
+  }, [canvasRef, refresh]);
+
+  const shareScreenshot = useCallback(async () => {
+    if (!canvasRef.current) return;
+
+    // Capture the canvas content as a Base64 string
+    const dataUrl = canvasRef.current.toDataURL("image/png");
+
+    // Convert the Base64 string to a Blob
+    const response = await fetch(dataUrl);
+    const blob = await response.blob();
+
+    // Create a downloadable file
+    const file = new File([blob], "canvas-screenshot.png", { type: "image/png" });
+
+    // Check if the Web Share API is supported
+    if (navigator.share && navigator.canShare({ files: [file] })) {
+      try {
+        // Share the image as a file
+        await navigator.share({
+          files: [file],
+          title: `Spielzettel Screenshot ${name}`,
+          //text: "",
+        });
+        console.log("Screenshot shared successfully!");
+      } catch (error) {
+        console.error("Error sharing screenshot:", error);
+      }
+    } else {
+      // Fall back to downloading the image
+      console.warn("Web Share API not supported on this device. Falling back to download.");
+      const link = document.createElement("a");
+      link.href = dataUrl;
+      link.download = "canvas-screenshot.png";
+      link.click();
+    }
+  }, [canvasRef, name]);
+
+  const fileUpload = useMemo(() => <>
+    <input
+      type="file"
+      id="file-upload"
+      style={styles.fileInput}
+      onChange={handleFileUpload}
+    />
+    <label htmlFor="file-upload" style={styles.button}>
+      Upload File
+    </label>
+  </>, []);
+
+  const closeOverlay = useCallback((event: ReactMouseEvent<HTMLDivElement, MouseEvent>) => {
+    // Check if the click target is not a button or its child
+    if (event.target === event.currentTarget) {
+      setOverlayVisible(false);
+    }
+  }, []);
+
+  const toggleDebug = useCallback(() => {
+    if (!debug) {
+      window.alert(debugString);
+    }
+    setDebug(!debug);
+  }, [debug, debugString]);
+
   return (
     <div style={styles.container}>
-      <div style={styles.controls}>
-        <input
-          type="file"
-          id="file-upload"
-          style={styles.fileInput}
-          onChange={handleFileUpload}
-        />
-        <label htmlFor="file-upload" style={styles.button}>
-          Upload File
-        </label>
-        {file !== null ? (
-          <button style={styles.button} onClick={resetCanvas}>
-            Reset
-          </button>
-        ) : undefined}
-      </div>
+      {file === null && fileUpload}
+      {/* Top-right button */}
+      {file !== null && (
+        <button style={styles.topRightButton} onClick={toggleOverlay}>
+          {overlayVisible ? "Close" : "Menu"}
+        </button>
+      )}
+
+      {/* Overlay with controls */}
+      {overlayVisible && file !== null && (
+        <div style={styles.overlay}
+          onClick={closeOverlay}>
+          <div style={styles.controls}>
+            {fileUpload}
+            <button style={styles.button} onClick={resetCanvas}>
+              Reset {name}
+            </button>
+            <button style={styles.button} onClick={shareScreenshot}>
+              Share Screenshot
+            </button>
+            <button style={styles.button} onClick={toggleDebug}>
+              Debug: {debug ? "ON" : "OFF"}
+            </button>
+          </div>
+        </div>
+      )}
+
+      {/* Canvas */}
       {spielZettelData !== null ? (
         <canvas
           ref={canvasRef}
@@ -252,11 +291,11 @@ const styles = {
     color: "white",
     height: "100vh",
     overflow: "hidden",
+    position: "relative",
   },
   controls: {
-    position: "absolute",
-    top: "10px",
     display: "flex",
+    flexDirection: "column",
     gap: "10px",
   },
   fileInput: {
@@ -272,9 +311,32 @@ const styles = {
     fontWeight: "bold",
   },
   canvas: {
-    marginTop: "50px",
     backgroundColor: "black",
     display: "block",
+  },
+  topRightButton: {
+    position: "absolute",
+    top: "10px",
+    right: "10px",
+    padding: "10px 20px",
+    backgroundColor: "#FF6347",
+    color: "#fff",
+    borderRadius: "5px",
+    cursor: "pointer",
+    fontSize: "16px",
+    fontWeight: "bold",
+  },
+  overlay: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    backgroundColor: "rgba(0, 0, 0, 0.8)",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    zIndex: 10,
   },
 } as const;
 
