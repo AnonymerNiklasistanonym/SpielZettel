bcf9fe775458a6f7b94a91f5d0078f5703ac645a
Niklas <niklas.mikeler@gmail.com>
2025-01-24 07:24:50 +0100

    Update version

--- spiel-zettel/src/app/helper/info.ts
+++ spiel-zettel/src/app/helper/info.ts
@@ -1,5 +1,5 @@
 export const name = "SpielZettel";
-export const version = "v0.0.40";
+export const version = "v0.0.41";
 
 export const urlGitRepo =
   "https://github.com/AnonymerNiklasistanonym/SpielZettel";
